2ebf878a61e265cf276d707d90e440acbb5f97d3
Piotr Sarna <sarna@scylladb.com>
2022-06-14 14:43:54 +0200

    Add calloc and standardize output

--- preload.cc
+++ preload.cc
@@ -1,10 +1,10 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
-#include <ostream>
 #endif
 
 #include <cstdint>
 #include <iostream>
+#include <iomanip>
 #include <dlfcn.h>
 
 #define TRY_PRELOAD(func) \
@@ -18,20 +18,30 @@
     } while (0)
 
 static struct stats {
-    uint64_t mallocs;
-    uint64_t reallocs;
-    uint64_t frees;
+    uint64_t mallocs = 0;
+    uint64_t reallocs = 0;
+    uint64_t callocs = 0;
+    uint64_t frees = 0;
 } _stats;
 
 void __attribute__((destructor)) print_stats() {
-    std::cout << "Mallocs: " << _stats.mallocs << std::endl
-              << "Reallocs: " << _stats.reallocs << std::endl
-              << "Frees: " << _stats.frees << std::endl
+    std::cout
+        << "----------" << std::endl
+        << "Alloc stats" << std::endl
+        << "----------" << std::endl
+        << "Mallocs:  " << std::setw(12) << _stats.mallocs << std::endl
+        << "Reallocs: " << std::setw(12) << _stats.reallocs << std::endl
+        << "Callocs:  " << std::setw(12) << _stats.callocs << std::endl
+        << "----------" << std::endl
+        << "sum =     " << std::setw(12) << (_stats.mallocs + _stats.reallocs + _stats.callocs) << std::endl
+        << "----------" << std::endl
+        << "Frees:    " << std::setw(12) << _stats.frees << std::endl
     ;
 }
 
 static void* (*real_malloc)(size_t) = nullptr;
 static void* (*real_realloc)(void*, size_t) = nullptr;
+static void* (*real_calloc)(size_t, size_t) = nullptr;
 static void (*real_free)(void*) = nullptr;
 
 void *malloc(size_t size)
@@ -47,6 +57,13 @@ void *realloc(void* addr, size_t size) {
 
     _stats.reallocs++;
     return real_realloc(addr, size);
+}
+
+void *calloc(size_t nmemb, size_t size) {
+    TRY_PRELOAD(calloc);
+
+    _stats.callocs++;
+    return real_calloc(nmemb, size);
 }
 
 void free(void* addr) {
